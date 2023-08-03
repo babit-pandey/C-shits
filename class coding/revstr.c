b03f7e84cc384515095a4fb9c40d8c250587416f
Babit Pandey <65017151+babit-pandey@users.noreply.github.com>
2023-08-03 23:34:05 +0545

    Add files via upload

---
+++ class coding/revstr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+void reverse(char word[]);
+int main()
+{
+    char input[100];
+    printf("Enter a string: ");
+    scanf("%s", input);
+    reverse(input);
+    printf("Reversed string: %s\n", input);
+    return 0;
+}
+void reverse(char word[])
+{
+    int length = strlen(word);
+    for (int i = 0; i < length / 2; i++)
+    {
+        char temp = word[i];
+        word[i] = word[length - i - 1];
+        word[length - i - 1] = temp;
+    }
+}
---
+++ class coding/strcount.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int countWords(char str[]);
+int main()
+{
+    char str[100];
+    printf("Enter your text: ");
+    scanf("%[^\n]", str);
+    int numWords = countWords(str);
+    printf("The input string contains %d words.\n", numWords);
+    return 0;
+}
+int countWords(char str[])
+{
+    int i, count = 0;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            count++;
+        }
+    }
+    count++;
+    return count;
+}
